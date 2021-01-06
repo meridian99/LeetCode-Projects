db781fc20753b430fa0273d333262c58dc83409e
Meridian <meridianmcc@gmail.com>
2021-01-06 10:53:56 -0800

    Initial Commit 2

---
+++ main.py
@@ -0,0 +1,21 @@
+class Solution:
+    @staticmethod
+    def trailingzeroes(n: int) -> int:
+
+        # Calculate the factorial value n!
+        counter = 1
+        for number in range(1, n + 1):
+            counter *= number
+
+        # Convert to string
+        reverse = str(counter)[::-1]
+
+        # Count the zeros
+        numberofzeros = 0
+        for numeral in reverse:
+            if numeral == "0":
+                numberofzeros += 1
+            else:
+                break
+
+        return numberofzeros
